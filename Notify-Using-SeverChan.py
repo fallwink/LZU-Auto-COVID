35d4f0c140117a6b565bb1c75a47ab88aa4f4de8
Hollow Man <hollowman@hollowman.ml>
2021-01-11 14:05:29 +0800

    Latest push
    Signed-off-by: Hollow Man <hollowman@hollowman.ml>

--- Notify-Using-SeverChan.py
+++ Notify-Using-SeverChan.py
@@ -10,10 +10,14 @@ import urllib.parse
 sckey = os.environ['SERVERCHANSCKEY']
 info = ""
 if sckey:
-    with open("information.txt") as infofile:
-        info = urllib.parse.urlencode(infofile.read())
-    if info:
-        info = "工作流或者打卡程序存在问题，请查看运行记录并提交issue!"
+    try:
+        with open("information.txt") as infofile:
+            info = urllib.parse.urlencode(infofile.read())
+    except Exception:
+        pass
+    finally:
+        if not info:
+            info = "工作流或者打卡程序存在问题，请查看运行记录并提交issue!"
         res = requests.get("http://sc.ftqq.com/" + sckey +
                         ".send?text=%E5%85%B0%E5%B7%9E%E5%A4%A7%E5%AD%A6%E8%87%AA%E5%8A%A8%E5%81%A5%E5%BA%B7%E6%89%93%E5%8D%A1%E8%BF%90%E8%A1%8C%E7%BB%93%E6%9E%9C&desp=" + info)
         result = json.loads(res.text)
