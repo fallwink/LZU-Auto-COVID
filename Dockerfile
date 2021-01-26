7ad62cd40defb8f0d053f3ebb52b8810a8ede686
Hollow Man <hollowman@hollowman.ml>
2021-01-26 20:11:19 +0800

    Latest push
    Signed-off-by: Hollow Man <hollowman@hollowman.ml>

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM python:latest
+
+MAINTAINER Hollow Man <hollowman@hollowman.ml>
+
+LABEL version="1.0.0"
+LABEL repository="https://github.com/HollowMan6/LZU-Auto-COVID-Health-Report"
+LABEL homepage="https://hollowman.ml/"
+LABEL maintainer="Hollow Man <hollowman@hollowman.ml>"
+
+COPY entrypoint.sh /entrypoint.sh
+COPY LZU-Auto-COVID-Health-Report.py /LZU-Auto-COVID-Health-Report.py
+COPY Notify-Using-SeverChan-Or-PushPlus.py /Notify-Using-SeverChan-Or-PushPlus.py
+COPY requirements.txt /requirements.txt
+
+ENV TZ Asia/Shanghai
+
+RUN pip install --upgrade pip
+RUN pip install --no-cache-dir -r /requirements.txt
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Setup Environmental Variables
+env -0 | while IFS='=' read -r -d '' n v; do
+    if [ ${n#INPUT_} != $n ]
+    then
+        echo "export ${n#INPUT_}=$v" >> /envar
+    fi
+done
+source /envar
+
+# Main Program Execution
+if [ -n $DELAYS ]
+then
+    echo "---Wait for $DELAYS---"
+    sleep $DELAYS
+fi
+echo "---Auto COVID Health Report---"
+if python /LZU-Auto-COVID-Health-Report.py >> /information.txt && cat /information.txt;
+then
+    echo "---Notify Success Using ServerChan or PushPlus---"
+    python /Notify-Using-SeverChan-Or-PushPlus.py success
+else
+    echo "---Error logging and Notifying Using ServerChan or PushPlus---"
+    python /Notify-Using-SeverChan-Or-PushPlus.py failure && cat /information.txt
+    exit 1
+fi
