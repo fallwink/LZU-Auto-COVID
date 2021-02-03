bb41d0e379cbe3a496e5924dc7d31937d51ab7b1
Hollow Man <hollowman@hollowman.ml>
2021-02-03 21:35:15 +0800

    Latest push
    Signed-off-by: Hollow Man <hollowman@hollowman.ml>

--- Notify-Result.py
+++ Notify-Result.py
@@ -26,30 +26,35 @@ if sckey:
     except Exception as e:
         print(e)
     finally:
-        if not info:
-            info = "工作流或者打卡程序存在问题，请查看运行记录并提交issue!"
-            status = "failure"
-        message = "%E5%A4%B1%E8%B4%A5%E2%9C%96"
-        if status == "success":
-            message = "%E6%88%90%E5%8A%9F%E2%9C%94"
-        host = "https://sc.ftqq.com/"
-        user = ""
-        if openid:
-            host = "https://sctapi.ftqq.com/"
-            if openid != "0":
-                user = "&openid=" + openid
-        res = requests.get(host + sckey + ".send?text=" + message +
-                           "%E5%85%B0%E5%B7%9E%E5%A4%A7%E5%AD%A6%E8%87%AA%E5%8A%A8%E5%81%A5%E5%BA%B7%E6%89%93%E5%8D%A1&desp=" + info + user)
-        result = json.loads(res.text)
-        if not openid and result['errno'] == 0:
-            print("成功通过Sever酱将结果通知给用户!")
-        elif openid and result['data']['errno'] == 0:
-            if openid == "0":
-                print("成功通过Sever酱将结果通知到测试公众号的创建用户!")
+        try:
+            if not info:
+                info = "工作流或者打卡程序存在问题，请查看运行记录并提交issue!"
+                status = "failure"
+            message = "%E5%A4%B1%E8%B4%A5%E2%9C%96"
+            if status == "success":
+                message = "%E6%88%90%E5%8A%9F%E2%9C%94"
+            host = "https://sc.ftqq.com/"
+            user = ""
+            if openid:
+                host = "https://sctapi.ftqq.com/"
+                if openid != "0":
+                    user = "&openid=" + openid
+            res = requests.get(host + sckey + ".send?text=" + message +
+                            "%E5%85%B0%E5%B7%9E%E5%A4%A7%E5%AD%A6%E8%87%AA%E5%8A%A8%E5%81%A5%E5%BA%B7%E6%89%93%E5%8D%A1&desp=" + info + user)
+            result = json.loads(res.text)
+            if not openid and result['errno'] == 0:
+                print("成功通过Sever酱将结果通知给用户!")
+            elif openid and result['data']['errno'] == 0:
+                if openid == "0":
+                    print("成功通过Sever酱将结果通知到测试公众号的创建用户!")
+                else:
+                    print("成功通过Sever酱将结果通知到测试公众号的指定关注用户和创建用户!")
             else:
-                print("成功通过Sever酱将结果通知到测试公众号的指定关注用户和创建用户!")
-        else:
-            errorNotify += "Server酱推送错误: " + res.text + "\n"
+                errorNotify += "Server酱推送错误: " + res.text + "\n"
+        except Exception as e:
+            print(e)
+            errorNotify += "Server酱推送错误!\n"
+
 else:
     print("未设置SERVERCHANSCKEY，尝试使用PushPlus...")
 
@@ -62,22 +67,26 @@ if pptoken:
     except Exception as e:
         print(e)
     finally:
-        if not info:
-            info = "工作流或者打卡程序存在问题，请查看运行记录并提交issue!"
-            status = "failure"
-        message = "%E5%A4%B1%E8%B4%A5%E2%9C%96"
-        if status == "success":
-            message = "%E6%88%90%E5%8A%9F%E2%9C%94"
-        host = "http://pushplus.hxtrip.com/"
-        user = ""
-        res = requests.get(host + "send?token=" + pptoken + "&title=" + message +
-                           "%E5%85%B0%E5%B7%9E%E5%A4%A7%E5%AD%A6%E8%87%AA%E5%8A%A8%E5%81%A5%E5%BA%B7%E6%89%93%E5%8D%A1&content=" + info
-                           + "&template=html&topic=" + pptopic)
-        result = json.loads(res.text)
-        if result['code'] == 200:
-            print("成功通过PushPlus将结果通知给相关用户!")
-        else:
-            errorNotify += "PushPlus推送错误: " + res.text + "\n"
+        try:
+            if not info:
+                info = "工作流或者打卡程序存在问题，请查看运行记录并提交issue!"
+                status = "failure"
+            message = "%E5%A4%B1%E8%B4%A5%E2%9C%96"
+            if status == "success":
+                message = "%E6%88%90%E5%8A%9F%E2%9C%94"
+            host = "http://pushplus.hxtrip.com/"
+            user = ""
+            res = requests.get(host + "send?token=" + pptoken + "&title=" + message +
+                            "%E5%85%B0%E5%B7%9E%E5%A4%A7%E5%AD%A6%E8%87%AA%E5%8A%A8%E5%81%A5%E5%BA%B7%E6%89%93%E5%8D%A1&content=" + info
+                            + "&template=html&topic=" + pptopic)
+            result = json.loads(res.text)
+            if result['code'] == 200:
+                print("成功通过PushPlus将结果通知给相关用户!")
+            else:
+                errorNotify += "PushPlus推送错误: " + res.text + "\n"
+        except Exception as e:
+            print(e)
+            errorNotify += "PushPlus推送错误!\n"
 else:
     print("未设置PPTOKEN，尝试推送到Telegram...")
 
@@ -92,22 +101,26 @@ if tgbottoken:
         except Exception as e:
             print(e)
         finally:
-            if not info:
-                info = "工作流或者打卡程序存在问题，请查看运行记录并提交issue!"
-                status = "failure"
-            message = "%E5%A4%B1%E8%B4%A5%E2%9C%96"
-            if status == "success":
-                message = "%E6%88%90%E5%8A%9F%E2%9C%94"
-            host = "https://api.telegram.org/bot"
-            user = ""
-            res = requests.get(host + tgbottoken + "/sendMessage?chat_id=" + tgchatid + "&text=*%20_%20__" + message +
-                               "%E5%85%B0%E5%B7%9E%E5%A4%A7%E5%AD%A6%E8%87%AA%E5%8A%A8%E5%81%A5%E5%BA%B7%E6%89%93%E5%8D%A1__%20_%20*" + info
-                               + "&parse_mode=MarkdownV2")
-            result = json.loads(res.text)
-            if result['ok']:
-                print("成功通过Telegram将结果通知给相关用户!")
-            else:
-                errorNotify += "Telegram推送错误: " + res.text
+            try:
+                if not info:
+                    info = "工作流或者打卡程序存在问题，请查看运行记录并提交issue!"
+                    status = "failure"
+                message = "%E5%A4%B1%E8%B4%A5%E2%9C%96"
+                if status == "success":
+                    message = "%E6%88%90%E5%8A%9F%E2%9C%94"
+                host = "https://api.telegram.org/bot"
+                user = ""
+                res = requests.get(host + tgbottoken + "/sendMessage?chat_id=" + tgchatid + "&text=*%20_%20__" + message +
+                                "%E5%85%B0%E5%B7%9E%E5%A4%A7%E5%AD%A6%E8%87%AA%E5%8A%A8%E5%81%A5%E5%BA%B7%E6%89%93%E5%8D%A1__%20_%20*" + info
+                                + "&parse_mode=MarkdownV2")
+                result = json.loads(res.text)
+                if result['ok']:
+                    print("成功通过Telegram将结果通知给相关用户!")
+                else:
+                    errorNotify += "Telegram推送错误: " + res.text
+            except Exception as e:
+                print(e)
+                errorNotify += "Telegram推送错误!\n"
     else:
         print("未设置TGCHATID，无法推送到Telegram!")
 else:
