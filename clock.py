e53e297628a3f3690aad740d349bcfb3f51dd70b
Hollow Man <hollowman@hollowman.ml>
2021-02-17 16:19:38 +0800

    Latest push
    Signed-off-by: Hollow Man <hollowman@hollowman.ml>

--- clock.py
+++ clock.py
@@ -9,14 +9,15 @@ import os
 
 def job_function():
     failure = False
-    if os.system("python LZU-Auto-COVID-Health-Report.py >> information.txt && cat information.txt >> logs.txt") == 0:
-        if os.system("python Notify-Result.py success >> logs.txt") != 0:
+    if os.system("python LZU-Auto-COVID-Health-Report.py >> information.txt && cat information.txt") == 0:
+        if os.system("python Notify-Result.py success") != 0:
             failure = True
     else:
         failure = True
         os.system(
-            "python Notify-Result.py failure >> logs.txt && cat information.txt >> logs.txt")
+            "python Notify-Result.py failure && cat information.txt")
     delays = os.environ['DELAYS']
+    os.system("rm information.txt")
     if failure and delays:
         os.system("echo 'Sleep for " + delays +
                   " and the health report will start again!")
