10f60229244fccba9eb16afae832dd08914091c5
Hollow Man <hollowman@hollowman.ml>
2021-07-24 07:54:01 +0800

    Latest push
    Signed-off-by: Hollow Man <hollowman@hollowman.ml>

--- helmChart/install-online.sh
+++ helmChart/install-online.sh
@@ -3,10 +3,10 @@
 kubectl create ns lzu-auto-covid-health-report
 # Add Repo
 helm repo add LZU-Auto-COVID-Health-Report http://hollowman.ml/LZU-Auto-COVID-Health-Report
-
+helm repo update
 # Install
 echo "---Enter your Actions Secrets, empty if it doesn't exists---"
-secrets=("cardID" "serverChanSCKey" "openID" "ppToken" "ppTopic" "tgBotToken" "tgChatID" "corpID" "corpSecret" "agentID" "subsInfo")
+secrets=("cardID" "serverChanSCKey" "ppToken" "ppTopic" "tgBotToken" "tgChatID" "corpID" "corpSecret" "agentID" "subsInfo")
 command="helm install lzu-auto-covid-health-report LZU-Auto-COVID-Health-Report/lzu-auto-covid-health-report --namespace lzu-auto-covid-health-report"
 for secret in ${secrets[*]}
 do
